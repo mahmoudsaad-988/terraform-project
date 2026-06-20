dd68e9dd4fc191f2c2142f33f333283621a7aa61
Mahmoud Elnaggar <118851778+mahmoudsaad-988@users.noreply.github.com>
2026-06-20 16:11:44 +0300

    Add Terraform provider configuration for AWS

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket       = "s3-state-123"
+    key          = "backend/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
