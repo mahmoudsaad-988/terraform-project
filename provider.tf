9c21a5281f2571eb949ab8ea24d53ae0bf6667d2
Mahmoud Elnaggar <118851778+mahmoudsaad-988@users.noreply.github.com>
2026-06-20 16:35:06 +0300

    Update provider.tf for Terraform version and locking
    Updated the required Terraform version and added a comment regarding S3 native locking.

--- provider.tf
+++ provider.tf
@@ -1,15 +1,19 @@
 terraform {
+  # Ensures the local engine binary supports the new native S3 native locking mechanism
+  required_version = ">= 1.10.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
     bucket       = "s3-state-123"
     key          = "backend/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = true
+    use_lockfile = true # Native S3 locking (requires Terraform 1.10+)
   }
 }
 
