47e1e08dbbd9c0c7035afb09ef87d14326dcfac7
Mahmoud Elnaggar <118851778+mahmoudsaad-988@users.noreply.github.com>
2026-06-20 16:33:26 +0300

    Refactor AWS resources in main.tf
    Updated various resource configurations and added lifecycle management for the database instance.

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_vpc" "AppVPC" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-
   tags = {
     Name = "AppVPC"
   }
@@ -14,29 +13,24 @@ resource "aws_subnet" "AppSubnet1" {
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
-
   tags = {
     Name = "AppSubnet1"
   }
 }
-
 
 resource "aws_subnet" "AppSubnet2" {
   vpc_id            = aws_vpc.AppVPC.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 
-
   tags = {
     Name = "AppSubnet2"
   }
 }
 
-
 resource "aws_security_group" "WebTrafficSG" {
   vpc_id = aws_vpc.AppVPC.id
   name   = "WebTrafficSG"
-
 
   ingress {
     from_port   = 22
@@ -45,14 +39,12 @@ resource "aws_security_group" "WebTrafficSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 
   ingress {
     from_port   = 443
@@ -61,14 +53,12 @@ resource "aws_security_group" "WebTrafficSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   ingress {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
 
   egress {
     from_port   = 0
@@ -77,41 +67,34 @@ resource "aws_security_group" "WebTrafficSG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-
   tags = {
     Name = "WebTrafficSG"
   }
 }
 
 resource "aws_network_interface" "nw-interface1" {
-  subnet_id = aws_subnet.AppSubnet1.id
+  subnet_id       = aws_subnet.AppSubnet1.id
   security_groups = [aws_security_group.WebTrafficSG.id]
   tags = {
-    Name        = "nw-interface1"
+    Name = "nw-interface1"
   }  
 }
-
 
 resource "aws_network_interface" "nw-interface2" {
-  subnet_id = aws_subnet.AppSubnet2.id
+  subnet_id       = aws_subnet.AppSubnet2.id
   security_groups = [aws_security_group.WebTrafficSG.id]
   tags = {
-    Name        = "nw-interface2"
+    Name = "nw-interface2"
   }  
 }
-
-
-
 
 resource "aws_internet_gateway" "AppIGW" {
   vpc_id = aws_vpc.AppVPC.id
-
 
   tags = {
     Name = "AppInternetGateway"
   }
 }
-
 
 resource "aws_route_table" "AppRouteTable" {
   vpc_id = aws_vpc.AppVPC.id
@@ -120,13 +103,9 @@ resource "aws_route_table" "AppRouteTable" {
   }
 }
 
-
 output "route_table_ID" {
   value = aws_route_table.AppRouteTable.id
 }
-
-
-
 
 resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.AppRouteTable.id
@@ -134,128 +113,98 @@ resource "aws_route" "internet_access" {
   gateway_id             = aws_internet_gateway.AppIGW.id
 }
 
-
-
-
 resource "aws_route_table_association" "AppSubnet1_association" {
   subnet_id      = aws_subnet.AppSubnet1.id
   route_table_id = aws_route_table.AppRouteTable.id
 }
-
 
 resource "aws_route_table_association" "AppSubnet2_association" {
   subnet_id      = aws_subnet.AppSubnet2.id
   route_table_id = aws_route_table.AppRouteTable.id
 }
 
-
 resource "aws_eip" "public_ip1" {
-
-
   network_interface = aws_network_interface.nw-interface1.id
-    tags = {
-        Name = "ElasticIP-1"
+  tags = {
+    Name = "ElasticIP-1"
   }
 }
-
 
 resource "aws_eip" "public_ip2" {
   network_interface = aws_network_interface.nw-interface2.id
-    tags = {
-        Name = "ElasticIP-2"
+  tags = {
+    Name = "ElasticIP-2"
   }
 }
 
-
 resource "aws_instance" "WebServer1" {
-  ami             = "ami-06c68f701d8090592"
-  instance_type   = "t2.micro"
-
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
 
   network_interface {
     network_interface_id = aws_network_interface.nw-interface1.id
-    device_index = 0
+    device_index         = 0
   }
 
-
   key_name = "my-ec2-key"
-
 
   tags = {
     Name = "WebServer1"
   }
 }
 
-
 resource "aws_instance" "WebServer2" {
-  ami             = "ami-06c68f701d8090592"
-  instance_type   = "t2.micro"
-
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
 
   network_interface {
     network_interface_id = aws_network_interface.nw-interface2.id
-    device_index = 0
+    device_index         = 0
   }
 
-
   key_name = "my-ec2-key"
-
 
   tags = {
     Name = "WebServer2"
   }
 }
 
-
-
-
 output "instance1_id" {
   value = aws_instance.WebServer1.id
 }
-
 
 output "instance2_id" {
   value = aws_instance.WebServer2.id
 }
 
-
-
-
 resource "aws_db_subnet_group" "app_db_subnet_group" {
   name       = "app-db-subnet-group"
   subnet_ids = [aws_subnet.AppSubnet1.id, aws_subnet.AppSubnet2.id]  
-
 
   tags = {
     Name = "AppDBSubnetGroup"
   }
 }
 
-
-
-
 resource "aws_db_instance" "app_database" {
-  allocated_storage    = 20
-  engine               = "mysql"
-  engine_version       = "8.0.45"  
-  instance_class       = "db.t3.micro"
-  identifier           = "appdatabase"
-  db_name              = "appdatabase"
-  username             = "admin"
-  password             =  var.db_password 
-  publicly_accessible     = true
-  db_subnet_group_name = aws_db_subnet_group.app_db_subnet_group.name
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0.45"  
+  instance_class         = "db.t3.micro"
+  identifier             = "appdatabase"
+  db_name                = "appdatabase"
+  username               = "admin"
+  password               = var.db_password
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.app_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.WebTrafficSG.id]
 
+  lifecycle {
+    ignore_changes = [password]
+  }
 
   tags = {
     Name = "AppDatabase"
   }
 }
-
-
-
-
-
-
-
